fa6eee1ff8f645e9439e3283929cac137f11f0fa
Jerzy Wieczorek <jerzywie@razzydo.co.uk>
2021-05-26 11:53:57 +0100

    Added start-line parameter to enable re-start from arbitrary line in xls.

--- src/basket_add/core.clj
+++ src/basket_add/core.clj
@@ -5,20 +5,23 @@
   (:gen-class))
 
 (def throttle-ms 100)
-(def start-line-number 3)
-(def counter (atom start-line-number))
+(def default-start-line 3)
+(def counter (atom 0))
 
 (defn -main
   "Upload Albany order spreadsheet to Essential basket"
   [& args]
   (if (< (count args) 3)
-    (println "Usage parameters: spreadsheet username password")
+    (println "Usage parameters: spreadsheet username password [start-line-number]")
     (try
       (let [xls (first args)
-           u (second args)
-           p (nth args 2)]
-       (println "Adding the contents of " xls " to essential basket...")
+            u (second args)
+            p (nth args 2)
+            start-line (if (= (count args) 4) (Integer. (nth args 3)) default-start-line)]
+       (println "Adding the contents of " xls " to essential basket. Starting at line " start-line "...")
+       (reset! counter start-line)
        (essential-login u p)
-       (add-all-to-basket (get-order xls) throttle-ms counter)
-       (println (- @counter start-line-number) "items added to basket. Balances are ex-VAT."))
+       (add-all-to-basket (get-order xls start-line) throttle-ms counter)
+       (println (- @counter start-line) "items added to basket. Balances are ex-VAT.")
+       (if (not= start-line default-start-line) (println "Click the \"Update all\" button on the Essential CurrentBasket page to get the basket into a consistent state.")))
       (catch Exception e (str "Error: " (.getMessage e))))))
--- src/basket_add/xls.clj
+++ src/basket_add/xls.clj
@@ -4,7 +4,7 @@
 (def order-cols {:A :code :B :description :C :case-size :D :essential-cases})
 
 (defn get-order
- [spreadsheet-name]
+ [spreadsheet-name start-line]
  (let [wb (load-workbook spreadsheet-name)
        order-sheet (select-sheet "Collated Order" wb)]
-   (drop 2 (select-columns order-cols order-sheet))))
+   (drop (- start-line 1) (select-columns order-cols order-sheet))))
