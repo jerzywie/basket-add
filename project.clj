3dc454c45f619ed9c1af31f48bb1642540b150e1
Jerzy Wieczorek <jerzywie@razzydo.co.uk>
2021-05-26 11:56:30 +0100

    Bump version. Add uberjar name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject basket-add "0.1.0-SNAPSHOT"
+(defproject basket-add "0.2.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
@@ -10,4 +10,5 @@
                  [dk.ative/docjure "1.10.0"]]
   :main ^:skip-aot basket-add.core
   :target-path "target/%s"
+  :uberjar-name "basket-add.jar"
   :profiles {:uberjar {:aot :all}})
