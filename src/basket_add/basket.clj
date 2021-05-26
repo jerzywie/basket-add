0b44f2d3f5dbf16da1d03ea738ab0ac7d9126b55
Jerzy Wieczorek <jerzywie@razzydo.co.uk>
2021-05-26 11:07:53 +0100

    Process response and provide more user feedback

--- src/basket_add/basket.clj
+++ src/basket_add/basket.clj
@@ -1,19 +1,38 @@
 (ns basket-add.basket
   (:require [basket-add.session :refer [cs]]
-            [clj-http.client :as client]))
+            [clj-http.client :as client]
+            [clojure.string :as s]))
 
 
-(def add-url "http://www.essential-trading.coop/extranet/do/___addItem.aspx?code=%s&qnt=%f&line=0&dojo.preventCache=%d")
+(def add-url "http://www.essential-trading.coop/extranet/do/___addItem.aspx?code=%s&qnt=%f&line=0&dojo.preventCache=%s")
+
+(defn process-body
+  "Extract desired information from body.
+   Assumes a body returned when HTTP Status = 200 (OK), for example:
+  99 lines in basket<br />Balance: &pound;1,711.81<br/><a title=\"View the contents of your basket\" href=\"/extranet/CurrentBasket.aspx\">View basket</a>"
+  [body]
+  (let [[lines raw-balance] (s/split body #"<br[ ]*/>")
+        balance (s/replace raw-balance "&pound;" "£")]
+    (str lines ". " balance)))
 
 (defn add-to-basket
-  [code quantity]
-  (println "Adding code:" code "qty:" quantity)
-  (let [cachebuster (System/currentTimeMillis)
-        item-url (format add-url code quantity cachebuster)
-        resp (client/get item-url {:cookie-store cs})]
-    resp))
+  [code quantity counter]
+
+  (try
+    ((print "Line "@counter ": Adding code:" code "qty:" quantity)
+     (let [cachebuster (str (System/currentTimeMillis) @counter)
+           item-url (format add-url code quantity cachebuster)
+           resp (client/get item-url {:cookie-store cs})
+           {status :status body :body} resp]
+       (println " ... Status " (if (= status 200) (str "OK. " (process-body body)) (str status " ****** ERROR" body)))
+       (swap! counter inc)
+       status))
+    (catch Exception e (str "Error: " (.getMessage e)))))
 
 (defn add-all-to-basket
-  [order-items throttle-ms]
+  [order-items throttle-ms counter]
   (dorun
-   (map (fn [{:keys [code essential-cases]}] (add-to-basket code essential-cases) (Thread/sleep throttle-ms)) order-items)))
+   (map (fn [{:keys [code essential-cases]}]
+          (add-to-basket code essential-cases counter)
+          (Thread/sleep throttle-ms))
+        order-items)))
--- src/basket_add/core.clj
+++ src/basket_add/core.clj
@@ -4,6 +4,10 @@
             [basket-add.xls :refer [get-order]])
   (:gen-class))
 
+(def throttle-ms 100)
+(def start-line-number 3)
+(def counter (atom start-line-number))
+
 (defn -main
   "Upload Albany order spreadsheet to Essential basket"
   [& args]
@@ -15,5 +19,6 @@
            p (nth args 2)]
        (println "Adding the contents of " xls " to essential basket...")
        (essential-login u p)
-       (add-all-to-basket (get-order xls) 100))
+       (add-all-to-basket (get-order xls) throttle-ms counter)
+       (println (- @counter start-line-number) "items added to basket. Balances are ex-VAT."))
       (catch Exception e (str "Error: " (.getMessage e))))))
