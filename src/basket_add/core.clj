1b9da4667573e24b17f38c6243444e9c38b089c5
Jerzy Wieczorek <jerzywie@razzydo.co.uk>
2022-12-10 17:48:55 +0000

    Prompt for password to avoid information leakage.

--- src/basket_add/core.clj
+++ src/basket_add/core.clj
@@ -11,17 +11,20 @@
 (defn -main
   "Upload Albany order spreadsheet to Essential basket"
   [& args]
-  (if (< (count args) 3)
-    (println "Usage parameters: spreadsheet username password [start-line-number]")
+  (if (< (count args) 2)
+    (do (println "Usage parameters: spreadsheet username [start-line-number]")
+        (println "                  (Password will be prompted for.)"))
     (try
       (let [xls (first args)
-            u (second args)
-            p (nth args 2)
-            start-line (if (= (count args) 4) (Integer. (nth args 3)) default-start-line)]
-       (println "Adding the contents of " xls " to essential basket. Starting at line " start-line "...")
-       (reset! counter start-line)
-       (essential-login u p)
-       (add-all-to-basket (get-order xls start-line) throttle-ms counter)
-       (println (- @counter start-line) "items added to basket. Balances are ex-VAT.")
-       (if (not= start-line default-start-line) (println "Click the \"Update all\" button on the Essential CurrentBasket page to get the basket into a consistent state.")))
+            username (second args)
+            start-line (if (= (count args) 3) (Integer. (nth args 2)) default-start-line)]
+        (print "Enter password: ")
+        (flush)
+        (let [pwd-secret (read-line)]
+          (println "Adding the contents of " xls " to essential basket. Starting at line " start-line "...")
+          (reset! counter start-line)
+          (essential-login username pwd-secret)
+          (add-all-to-basket (get-order xls start-line) throttle-ms counter)
+          (println (- @counter start-line) "items added to basket. Balances are ex-VAT.")
+          (if (not= start-line default-start-line) (println "Click the \"Update all\" button on the Essential CurrentBasket page to get the basket into a consistent state."))))
       (catch Exception e (str "Error: " (.getMessage e))))))
